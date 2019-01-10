86d491465ce32aa8e2fcd0372cac7ef6eb2457a9
Kyle White <kywhi@microsoft.com>
2019-01-10 14:54:56 -0500

    Add informational version to assembly properties.

--- src/Xamarin.Android.Tools.AndroidSdk/Properties/AssemblyInfo.cs
+++ src/Xamarin.Android.Tools.AndroidSdk/Properties/AssemblyInfo.cs
@@ -12,4 +12,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion ("1.0.0.0")]
 [assembly: AssemblyFileVersion ("1.0.0.0")]
+[assembly: AssemblyInformationalVersion ("1.0.99.0")]
 
