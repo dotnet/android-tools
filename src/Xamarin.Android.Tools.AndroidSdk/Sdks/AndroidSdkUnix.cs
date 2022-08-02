1fca51fcd9f685d7f5c72ffebc22219cb06dac96
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-08-02 09:47:44 -0500

    One Path.Combine() calll

--- src/Xamarin.Android.Tools.AndroidSdk/Sdks/AndroidSdkUnix.cs
+++ src/Xamarin.Android.Tools.AndroidSdk/Sdks/AndroidSdkUnix.cs
@@ -223,8 +223,7 @@ namespace Xamarin.Android.Tools
 			get {
 				var p   = AppDomain.CurrentDomain.GetData (GetUnixConfigDirOverrideName)?.ToString ();
 				if (string.IsNullOrEmpty (p)) {
-					p   = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.UserProfile), ".config");
-					p   = Path.Combine (p, "xbuild");
+					p   = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.UserProfile), ".config", "xbuild");
 				}
 				return Path.Combine (p, "monodroid-config.xml");
 			}
