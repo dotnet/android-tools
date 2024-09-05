94273feaaf0394aae9d8ae040bca1014d980b7b7
Jonathan Pryor <jonpryor@vt.edu>
2024-09-04 20:06:36 -0400

    Update UnhandledExceptionLogger.cs
    Improve error message text.

--- src/Microsoft.Android.Build.BaseTasks/UnhandledExceptionLogger.cs
+++ src/Microsoft.Android.Build.BaseTasks/UnhandledExceptionLogger.cs
@@ -101,7 +101,7 @@ namespace Microsoft.Android.Build.Tasks
 				if (string.IsNullOrEmpty (processes)) {
 					continue;
 				}
-				return $"{processes}{Environment.NewLine}{ex.ToString ()}";
+				return $"The file '{path}' is locked by: {processes}.{Environment.NewLine}{ex.ToString ()}";
 			}
 			return ex.ToString ();
 		}
